e32793175512192cc0f7bbb5e8a6c03505058871
icsd-surf <cstomce@gmail.com>
2021-05-12 08:18:36 +0800

    feat: add RedisScheduler module;

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ module.exports = {
 	Crawler: require('./lib/Crawler'),
 	Requester: require('./lib/Requester'),
 	Scheduler: require('./lib/Scheduler'),
+	RedisScheduler: require('./lib/RedisScheduler'),
 	Task: require('./lib/Task')
 }
