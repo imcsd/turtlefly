95a69679a9110cec7e29f98445838860984d08a5
icsd-work <cstomce@gmail.com>
2021-09-30 11:12:04 +0800

    refactor: remove Scheduler, Tasker, TaskQueue module

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 module.exports = {
 	Crawler: require('./lib/Crawler'),
 	Requester: require('./lib/Requester'),
-	Scheduler: require('./lib/Scheduler'),
 	RedisScheduler: require('./lib/RedisScheduler'),
 	Task: require('./lib/Task')
 }
